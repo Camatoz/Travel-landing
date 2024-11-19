4bc6f167a55f2aa56894dc2c1f2fda15b74c0c45
Camatoz <yurabondik99@gmail.com>
2024-11-19 20:36:19 +0200

    Added responsive menu

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@
     }
   };
 })();
+
+// Burger handler
+
+(function () {
+  const burgerItem = document.querySelector(".burger");
+  const menu = document.querySelector(".header-nav");
+  const menuClose = document.querySelector(".header_nav_close");
+  burgerItem.addEventListener("click", () => {
+    menu.classList.add("header-nav_active");
+    // console.log("9");
+  });
+  menuClose.addEventListener("click", () => {
+    menu.classList.remove("header-nav_active");
+  });
+})();
